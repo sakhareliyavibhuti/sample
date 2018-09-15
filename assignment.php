963b566055740b682934f4103776ee79ae2129d6
sakhareliyavibhuti <43293237+sakhareliyavibhuti@users.noreply.github.com>
2018-09-15 16:03:21 +0530

    Repository

---
+++ assignment.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+</head>
+<h1 align="center">Login form</h1>
+<table border="2" align="center">
+<tr>
+<td>
+Username<input type="text" name="username" value="" />
+</td>
+</tr>
+
+<tr>
+<td>
+password<input type="password" name="password" value="" />
+</td>
+</tr>
+<tr>
+<td>
+<input type="button" name="submit" value="submit" />
+</td>
+</tr>
+</table>
+
+
+<body>
+</body>
+</html>
